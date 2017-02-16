f315b4c897b3b9bb1da5c8263dc4684b1dddd82e
rotoosoft <lyzkhqby@sina.com>
2017-02-16 18:34:18 +0800

    new

---
+++ javascripts/product.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rotoosoft-d04 on 2017/2/16.
+ */
+var itemData = {
+    name: '【买即送餐具套装】沃德百惠 保温饭盒保温桶多层可组合学生儿童便当盒不锈钢四层',
+    price: '29.90',
+    count: '120',
+    titlePics: ['images/shop.jpg', 'images/1.jpg', 'images/2.jpg'],
+    details: ['images/1.jpg', 'images/2.jpg']
+}
+
+
+new Vue({
+    el: "#product",
+    data: {
+        product: itemData
+    }
+})
